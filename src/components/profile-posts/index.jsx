8a0c13255312b83ceddd1377d0ec110c595355b7
McChug <mcchug2112@gmail.com>
2025-10-06 22:15:05 -0600

    Revert "3-333"
    This reverts commit 67b0c03bc044da0c520aaa4ab256b20cbc14517d.

--- src/components/profile-posts/index.jsx
+++ src/components/profile-posts/index.jsx
@@ -2,11 +2,8 @@ import './style.css';
 import { getProfileData } from '../../services/profile';
 import { useQuery } from '@tanstack/react-query';
 import getInitials from '../../helpers/getInitials';
-import { useState } from 'react';
 
 export const ProfilePosts = () => {
-  const [expandedPostById, setExpandedPostById] = useState(null);
-
   const { data, isLoading } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfileData,
@@ -24,10 +21,6 @@ export const ProfilePosts = () => {
     };
     return date.toLocaleDateString(undefined, options);
   }
-
-  const toggleExpand = postId => {
-    setExpandedPostById(expandedPostById === postId ? null : postId);
-  };
 
   if (isLoading) {
     return (
@@ -49,52 +42,38 @@ export const ProfilePosts = () => {
     );
   }
 
-  const { pinnedPosts } = data;
+  const { pinnedPost } = data;
+
+  const authorInitials = getInitials(
+    `${pinnedPost.authorFirstName} ${pinnedPost.authorLastName}`,
+  );
 
   return (
     <section id="profile-posts">
       <h2 className="page-heading-2">Pinned Posts</h2>
       <div className="profile-post-results">
-        {pinnedPosts.map((pinnedPost, index) => {
-          const authorInitials = getInitials(
-            `${pinnedPost.authorFirstName} ${pinnedPost.authorLastName}`,
-          );
-          const postId = `post-${index}`;
-
-          return (
-            <div
-              key={postId}
-              className={
-                'content-card ' +
-                (expandedPostById === postId ? 'expanded' : '')
-              }
-              onClick={() => toggleExpand(postId)}
-            >
-              <div className="post-author fade-in">
-                <div className="post-author-avatar fade-in">
-                  <span>{authorInitials}</span>
-                </div>
-                <div className="post-author-info fade-in">
-                  <p className="page-paragraph">
-                    {pinnedPost.authorFirstName} {pinnedPost.authorLastName}
-                  </p>
-                  <p className="page-micro">
-                    {pinnedPost.jobTitle} @ {pinnedPost.companyName}
-                  </p>
-                </div>
-                <div className="post-metadata">
-                  <p>{formatPublishDate(pinnedPost.publishDate)}</p>
-                  <p>
-                    {pinnedPost.city}, {pinnedPost.state}
-                  </p>
-                </div>
-              </div>
-              <p className="page-body post-content fade-in">
-                {pinnedPost.post}
+        <div className="content-card">
+          <div className="post-author fade-in">
+            <div className="post-author-avatar fade-in">
+              <span>{authorInitials}</span>
+            </div>
+            <div className="post-author-info fade-in">
+              <p className="page-paragraph">
+                {pinnedPost.authorFirstName} {pinnedPost.authorLastName}
+              </p>
+              <p className="page-micro">
+                {pinnedPost.jobTitle} @ {pinnedPost.companyName}
               </p>
             </div>
-          );
-        })}
+          </div>
+          <p className="page-body post-content fade-in">{pinnedPost.post}</p>
+          <div className="post-metadata">
+            <p>{formatPublishDate(pinnedPost.publishDate)}</p>
+            <p>
+              {pinnedPost.city}, {pinnedPost.state}
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
